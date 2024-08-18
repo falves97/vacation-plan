cf43e671dd6393a262951a4f25b408624f850fa4
Fernando Braga Alves <fbragaalves97@gmail.com>
2024-08-18 16:01:16 -0300

    feat: resources e collections para holidayPlan e user

---
+++ app/Http/Resources/HolidayPlanResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HolidayPlanResource extends JsonResource
+{
+    public static $wrap = 'holiday_plan';
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'date' => $this->date->format('Y-m-d'),
+            'location' => $this->location,
+            'owner' => new UserResource($this->owner),
+            'participants' => new UserCollection($this->participants),
+        ];
+    }
+}
