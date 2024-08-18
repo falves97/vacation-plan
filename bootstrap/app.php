5e705bf6d1db3532b128e5285162e41ca6112440
Fernando Braga Alves <fbragaalves97@gmail.com>
2024-08-18 17:13:50 -0300

    refac: força resposta em json em caso de exceção nas rotas api/*

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Http\Middleware\EnforceJson;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,14 +13,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [EnforceJson::class]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'message' => 'Record not found.'
-                ], 404);
+        $exceptions->shouldRenderJsonWhen(
+            function (Request $request, Throwable $e) {
+                if ($request->is('api/*')) {
+                    return true;
+                }
+
+                return $request->expectsJson();
             }
-        });
+        );
     })->create();
